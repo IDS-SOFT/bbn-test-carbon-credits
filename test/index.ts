1a5797533ca44fb2a595578d12f961d286d43ea2
ayush24-eth <ayushtmsl611@gmail.com>
2023-10-12 11:30:37 +0530

    Updated contract and written testcases

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,85 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-import { expect } from "chai";
-import { ethers } from "hardhat";
+describe("EnvironmentalAsset", function () {
+  let EnvironmentalAsset: any;
+  let environmentalAsset: any;
+  let owner: any;
+  let recipient: any;
+  const assetName = "Carbon Credits";
+  const assetType = "Carbon Offset";
+  const initialSupply = 1000;
+  const assetPrice = 100;
+  const expirationDate = Math.floor(Date.now() / 1000) + 3600; // Set expiration date to 1 hour from now
 
-describe("EnvironmentalAsset", function () {});
+  beforeEach(async function () {
+    [owner, recipient] = await ethers.getSigners();
+
+    EnvironmentalAsset = await ethers.getContractFactory("EnvironmentalAsset");
+    environmentalAsset = await EnvironmentalAsset.deploy(
+      assetName,
+      assetType,
+      initialSupply,
+      assetPrice,
+      expirationDate
+    );
+    await environmentalAsset.deployed();
+  });
+
+  it("should mint assets and update balances", async function () {
+    const amountToMint = 100;
+    await environmentalAsset
+      .connect(owner)
+      .mintAsset(recipient.address, amountToMint);
+
+    const recipientBalance = await environmentalAsset.balances(
+      recipient.address
+    );
+    const availableSupply = await environmentalAsset.availableSupply();
+
+    expect(recipientBalance).to.equal(amountToMint);
+    expect(availableSupply).to.equal(initialSupply - amountToMint);
+  });
+
+  it("should transfer assets between addresses", async function () {
+    const amountToMint = 100;
+    await environmentalAsset
+      .connect(owner)
+      .mintAsset(recipient.address, amountToMint);
+
+    const amountToTransfer = 50;
+    await environmentalAsset
+      .connect(recipient)
+      .transferAsset(owner.address, amountToTransfer);
+
+    const recipientBalance = await environmentalAsset.balances(
+      recipient.address
+    );
+    const ownerBalance = await environmentalAsset.balances(owner.address);
+
+    expect(recipientBalance).to.equal(amountToMint - amountToTransfer);
+    expect(ownerBalance).to.equal(amountToTransfer);
+  });
+
+  it("should revoke assets after the expiration date", async function () {
+    // Increase the time to simulate the expiration date
+    await network.provider.send("evm_setNextBlockTimestamp", [
+      expirationDate + 1,
+    ]);
+    await network.provider.send("evm_mine");
+
+    let errorOccurred = false;
+
+    try {
+      await environmentalAsset.connect(owner).revokeAsset();
+      const ownerBalance = await environmentalAsset.balances(owner.address);
+      const availableSupply = await environmentalAsset.availableSupply;
+      expect(ownerBalance).to.equal(initialSupply);
+      expect(availableSupply).to.equal(0);
+    } catch (error) {
+      // If an error occurs, set the errorOccurred flag to true
+      errorOccurred = true;
+    }
+    expect(errorOccurred).to.equal(true);
+  });
+});
